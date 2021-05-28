caceca36ee74066223622ab847bb6ffb45526558
Saif Al-adilee <saifa9697@gmail.com>
2021-05-28 08:42:57 +0200

    Add project files.

---
+++ TempData_V0.2/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TempData_V0._2.Models;
+
+namespace TempData_V0._2.Controllers
+{
+    public class HomeController : Controller
+    {
+        // Get-Index
+        public IActionResult Index()
+        {
+            TempData["Counter"] = 1;
+
+            return View();
+        }
+
+        // Post-Index
+        [HttpPost]
+        public IActionResult Index(string click)
+        {
+            int x = (int)TempData.Peek("Counter");
+
+            if (click == "Add")
+            {
+                x++;
+            }
+            else 
+            {
+                if (x > 0)
+                {
+                    x--;
+                }
+                else
+                {
+                    ViewBag.Message = "The value can not be negative";
+                }
+            }
+
+            TempData["Counter"] = x;
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ TempData_V0.2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TempData_V0._2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
